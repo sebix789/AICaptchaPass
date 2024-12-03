e90f9ea85055c39b16c979fe6a04dfaa0cf8fda9
Sebastian <styralski98@gmail.com>
2024-12-03 19:18:36 +0100

    invoke tasks fixed on win and macos

--- tasks.py
+++ tasks.py
@@ -1,17 +1,27 @@
 from invoke import task
+import os
 
+def env_prefix(env_vars):
+    # Windows
+    if os.name == "nt":
+        return " ".join([f"set {key}={value} &&" for key, value in env_vars.items()])
+    # macOS/Linux
+    else:
+        return " ".join([f"{key}={value}" for key, value in env_vars.items()])
 
 @task
 def run_dev(c):
-    c.run("FLASK_ENV=development flask --app src.flaskr run --debug")
-
+    env = env_prefix({"FLASK_ENV": "development"})
+    print(env)
+    c.run(f"{env} flask --app src.flaskr run --debug")
 
 # ON PROD WE WILL SHOULD TO USE WSGI server
 @task
 def run_prod(c):
-    c.run("FLASK_ENV=production flask --app src.flaskr run")
-
+    env = env_prefix({"FLASK_ENV": "production"})
+    c.run(f"{env} flask --app src.flaskr run")
 
 @task
 def run_test(c):
-    c.run("PYTHONPATH=src FLASK_ENV=testing pytest src/tests")
+    env = env_prefix({"PYTHONPATH": "src", "FLASK_ENV": "testing"})
+    c.run(f"{env} pytest src/tests")
